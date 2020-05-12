0ed624744cf146839239db33076bc94fcd238afb
Gabriel Oliveira <gabriel.gondino@gmail.com>
2020-05-12 23:55:45 +0000

    initial commit

---
+++ src/main/groovy/ggo/sociallogin/SocialLoginApplication.groovy
@@ -0,0 +1,13 @@
+package ggo.sociallogin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SocialLoginApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SocialLoginApplication, args)
+	}
+
+}
